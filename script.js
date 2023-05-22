cb7b21c67cdf83b8f2431d8245687c11e6e6de83
Arijitnaskar12 <117929311+Arijitnaskar12@users.noreply.github.com>
2023-05-22 14:30:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 	// const divElement=document.createElement("div");
 	// divElement.id="sizeInfo";
 	const sizeElement=document.getElementsByTagName("h1")[0];
-	console.log(sizeElement.innerText);
+	// console.log(sizeElement.innerText);
 		// sizeElement.innerText=`Width:${window.innerWidth} and Height:${window.innerHeight}`;
 	// divElement.append(sizeElement);
 	// 	document.body.append(divElement);
