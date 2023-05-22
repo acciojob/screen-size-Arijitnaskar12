31d73112d46f5ff205e5b17dd8975d7dd95119fe
Arijitnaskar12 <117929311+Arijitnaskar12@users.noreply.github.com>
2023-05-22 14:25:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 	//your JS code here. If required.
-	const divElement=document.createElement("div");
-	divElement.id="sizeInfo";
-	const sizeElement=document.createElement("h1");
-		sizeElement.innerText=`Width:${window.innerWidth} and Height:${window.innerHeight}`;
-	divElement.append(sizeElement);
-		document.body.append(divElement);
+	// const divElement=document.createElement("div");
+	// divElement.id="sizeInfo";
+	const sizeElement=document.getElementsByTagName("h1")[0];
+	console.log(sizeElement.innerText);
+		// sizeElement.innerText=`Width:${window.innerWidth} and Height:${window.innerHeight}`;
+	// divElement.append(sizeElement);
+	// 	document.body.append(divElement);
 	window.onresize=function(){
 		sizeElement.innerText=`Width:${window.innerWidth} and Height:${window.innerHeight}`;
 	};
