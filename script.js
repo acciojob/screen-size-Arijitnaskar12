b2f5bef9fbf17307ab3e96445d5c09fa9fe16af7
Arijitnaskar12 <117929311+Arijitnaskar12@users.noreply.github.com>
2023-05-22 14:02:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-//your JS code here. If required.
-const divElement=document.createElement("div");
-divElement.id="sizeInfo";
-const sizeElement=document.createElement("h1");
-sizeElement.ineertext=`Width:${window.innerWidth} and Height:${window.innerHeight}`;
-divElement.append(sizeElement);
-	document.body.append(divElement);
-window.onresize=function(){
-	sizeElement.ineerText=`Width:${window.innerWidth} and Height:${window.innerHeight}`;
-};
+	//your JS code here. If required.
+	const divElement=document.createElement("div");
+	divElement.id="sizeInfo";
+	const sizeElement=document.createElement("h1");
+		sizeElement.innerText=`Width:${window.innerWidth} and Height:${window.innerHeight}`;
+	divElement.append(sizeElement);
+		document.body.append(divElement);
+	window.onresize=function(){
+		sizeElement.innerText=`Width:${window.innerWidth} and Height:${window.innerHeight}`;
+	};
