6925d955433d482c72625a843b796a06ead54424
Arijitnaskar12 <117929311+Arijitnaskar12@users.noreply.github.com>
2023-05-22 13:13:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //your JS code here. If required.
 const divElement=document.createElement("div");
+divElement.id="sizeInfo";
 const sizeElement=document.createElement("h1");
 sizeElement.ineertext=`Width:${window.innerWidth} and Height:${window.innerHeight}`;
 divElement.append(sizeElement);
